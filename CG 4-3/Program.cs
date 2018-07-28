5b24bd628752496c2b93339133a281bdb49a7fe7
Allison <allison.hix86@gmail.com>
2018-07-28 11:13:19 -0500

    homework

--- CG 4-3/Program.cs
+++ CG 4-3/Program.cs
@@ -6,7 +6,28 @@ namespace CG_4_3
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            // ask the user for a number value
+            Console.Write("Hello there, please enter a numer:  ");
+            
+            // convert the user input to an int 
+            string userValue = Console.ReadLine();
+            int Value = int.Parse(userValue);
+
+            // set total to zero for upcoming math
+            int total = 0;
+
+            // setting the loop to run from 2 to 20
+            for (int i = 2; i < 21; i++)
+            {
+                // multiplying the user input by the about run
+                Console.WriteLine(i * Value);
+                //for grand total we're saying add all i's * user input for grand total
+                total += i * Value;
+            }
+            // outputting the grand total
+            Console.Write(total);
+            Console.ReadLine();
+
         }
     }
 }
